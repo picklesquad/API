a81e31ee44ea7cb69751007e6abef6566221e1c7
Andri Kurniawan <andrikurniawan.id@gmail.com>
2016-05-16 16:21:27 +0700

    JUnit test init for Bank Service Test

---
+++ src/test/java/com/pickle/Service/BankServiceTest.java
@@ -0,0 +1,52 @@
+package com.pickle.Service;
+
+import com.pickle.Domain.BanksampahEntity;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by andrikurniawan.id@gmail.com on 5/16/2016.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class BankServiceTest {
+
+    @Autowired
+    @Mock
+    private BankService bankService;
+
+    @Mock
+    BanksampahEntity banksampahEntity;
+
+    @Test
+    public void validation() throws Exception {
+        when(bankService.validation("081245692312",
+                "syukrimullia")).thenReturn(banksampahEntity);
+    }
+
+    @Test
+    public void findById() throws Exception {
+        when(bankService.findById(1)).thenReturn(banksampahEntity);
+    }
+
+    @Test
+    public void searchByLocation() throws Exception {
+
+    }
+
+    @Test
+    public void save() throws Exception {
+
+    }
+
+}
